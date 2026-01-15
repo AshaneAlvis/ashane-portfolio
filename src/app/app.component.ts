c4090e0d432d9654e566775d51d512a70bac6e3f
Ashane Alvis <ashanealvis@gmail.com>
2026-01-15 19:37:00 +0530

    Removed unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
@@ -13,7 +12,6 @@ import { FooterComponent } from './components/footer/footer.component';
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
     HeaderComponent,
     AboutComponent,
     ExperienceComponent,
